5c3487b7e169294a6f4cb29a2e81f22a550625cf
Marco Hermo <marco@silverstripe.com>
2018-03-16 13:22:27 +1300

    Config Class

---
+++ code/Config.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Marcz\Search;
+
+use SilverStripe\Core\Injector\Injectable;
+use SilverStripe\Core\Config\Configurable;
+
+class Config
+{
+    use Injectable, Configurable;
+
+    public function details()
+    {
+        $details = [];
+        foreach (self::config()->get('indices') as $index) {
+            $config = [
+                'clients'    => [],
+                'indexName'  => $index['name'],
+                'dataObject' => $index['class'],
+            ];
+
+            foreach (self::config()->get('clients') as $client) {
+                $config['clients'][] = $client;
+            }
+
+            $details[] = $config;
+        }
+
+        return [
+            'configs'     => $details,
+            'indices'     => self::config()->get('indices'),
+            'clients'     => self::config()->get('clients'),
+            'page_length' => self::config()->get('page_length'),
+        ];
+    }
+}
